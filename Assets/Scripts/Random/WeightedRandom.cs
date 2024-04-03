5aebf6c4f3ce4692797b41a9baabfcc1ec0a1232
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2024-04-03 14:16:53 +0900

    [feat] Random => RandomNumberGenerator 변경

--- Assets/Scripts/Random/WeightedRandom.cs
+++ Assets/Scripts/Random/WeightedRandom.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 namespace Muks.WeightedRandom
@@ -7,12 +9,12 @@ namespace Muks.WeightedRandom
     /// <summary>가중치 랜덤 뽑기 시스템 클래스</summary>
     public class WeightedRandom<T>
     {
-        private Dictionary<T, int> _dic;
+        private Dictionary<T, int> _itemDic;
 
 
         public WeightedRandom()
         {
-            _dic = new Dictionary<T, int>();
+            _itemDic = new Dictionary<T, int>();
         }
 
 
@@ -26,18 +28,11 @@ namespace Muks.WeightedRandom
                 return;
             }
 
-            //만약 딕셔너리에 키가 존재하면?
-            if (_dic.ContainsKey(item))
-            {
-                //해당 키의 값의 수치를 변경한다.
-                _dic[item] += value;
-            }
-            //존재하지 않으면?
+            if (_itemDic.ContainsKey(item))
+                _itemDic[item] += value;
+
             else
-            {
-                //아이템을 추가한다.
-                _dic.Add(item, value);
-            }
+                _itemDic.Add(item, value);
         }
 
 
@@ -52,21 +47,14 @@ namespace Muks.WeightedRandom
             }
 
             //만약 딕셔너리에 키가 존재하면?
-            if (_dic.ContainsKey(item))
+            if (_itemDic.ContainsKey(item))
             {
                 //키의 값의 크기가 더 크면?
-                if (_dic[item] > value)
-                {
-                    //해당 키의 값의 수치를 변경한다.
-                    _dic[item] -= value;
-                }
-                //같거나 작으면?
-                else
-                {
-                    //삭제한다.
-                    Remove(item);
-                }
+                if (value < _itemDic[item])
+                    _itemDic[item] -= value;
 
+                else
+                    Remove(item);
             }
             else
             {
@@ -79,10 +67,10 @@ namespace Muks.WeightedRandom
         public void Remove(T item)
         {
             //만약 딕셔너리에 키가 존재하면?
-            if (_dic.ContainsKey(item))
+            if (_itemDic.ContainsKey(item))
             {
                 //해당 키의 데이터를 삭제한다.
-                _dic.Remove(item);
+                _itemDic.Remove(item);
             }
             else
             {
@@ -92,12 +80,12 @@ namespace Muks.WeightedRandom
 
 
         /// <summary>현재 리스트에 있는 아이템의 가중치를 모두 더해 반환</summary>
-        public int GetTotalWeight()
+        public int TotalWeight()
         {
             int totalWeight = 0;
 
             //딕셔너리에 입력된 모든 아이템 가중치 값을 더한다.
-            foreach (int value in _dic.Values)
+            foreach (int value in _itemDic.Values)
             {
                 totalWeight += value;
             }
@@ -110,9 +98,9 @@ namespace Muks.WeightedRandom
         public Dictionary<T, float> GetPercent()
         {
             Dictionary<T, float> _tempDic = new Dictionary<T, float>();
-            float totalWeight = GetTotalWeight();
+            float totalWeight = TotalWeight();
 
-            foreach (var item in _dic)
+            foreach (var item in _itemDic)
             {
                 _tempDic.Add(item.Key, item.Value / totalWeight);
             }
@@ -125,7 +113,7 @@ namespace Muks.WeightedRandom
         public T GetRamdomItemAfterSub()
         {
             //딕셔너리에 들어있는 아이템 갯수가 0이하면
-            if (_dic.Count <= 0)
+            if (_itemDic.Count <= 0)
             {
                 Debug.LogError("리스트에 아이템이 없습니다. 뽑기 불가능");
                 return default;
@@ -133,19 +121,23 @@ namespace Muks.WeightedRandom
 
             //총 가중치를 가져온다.
             int weight = 0;
-            int totalWeight = GetTotalWeight();
+            int totalWeight = TotalWeight();
 
             //총 가중치 값에 0~1f의 랜덤 값을 곱해 기준점을 구한다.
-            int pivot = Mathf.RoundToInt(totalWeight * Random.Range(0.0f, 1.0f));
+            int pivot = Mathf.RoundToInt(totalWeight * RandomRange(0.0f, 1.0f));
 
             //딕셔너리를 순회하며 가중치를 더하다 기준점 이상이 되면 그 아이템을 반환한다.
-            foreach (var item in _dic)
+            foreach (var item in _itemDic)
             {
                 weight += item.Value;
                 if (pivot <= weight)
                 {
                     
-                    _dic[item.Key] -= 1;
+                    _itemDic[item.Key] -= 1;
+
+                    if (_itemDic[item.Key] <= 0)
+                        Remove(item.Key);
+
                     return item.Key;
                 }
             }
@@ -157,21 +149,21 @@ namespace Muks.WeightedRandom
         public T GetRamdomItem()
         {
             //딕셔너리에 들어있는 아이템 갯수가 0이하면 리턴
-            if (_dic.Count <= 0)
+            if (_itemDic.Count <= 0)
             {
                 Debug.LogError("리스트에 아이템이 없습니다. 뽑기 불가능");
                 return default;
             }
 
             //총 가중치를 가져온다.
-            int totalWeight = GetTotalWeight();
+            int totalWeight = TotalWeight();
             int weight = 0;
 
             //총 가중치 값에 0~1f의 랜덤 값을 곱해 기준점을 구한다.
-            int pivot = Mathf.RoundToInt(totalWeight * Random.Range(0.0f, 1.0f));
+            int pivot = Mathf.RoundToInt(totalWeight * RandomRange(0.0f, 1.0f));
 
             //딕셔너리를 순회하며 가중치를 더하다 기준점 이상이 되면 그 아이템을 반환한다.
-            foreach (var item in _dic)
+            foreach (var item in _itemDic)
             {
                 weight += item.Value;
                 if (pivot <= weight)
@@ -186,7 +178,45 @@ namespace Muks.WeightedRandom
         /// <summary> 아이템 리스트를 반환 </summary>
         public Dictionary<T, int> GetList()
         {
-            return _dic;
+            return _itemDic;
         }
+
+
+        /// <summary>RandomNumberGenerator를 이용, 범위 안의 난수를 반환하는 함수</summary>
+        private int RandomRange(int min, int max)
+        {
+            if(max < min)
+            {
+                Debug.LogError("Min값이 Max값보다 높습니다.");
+                return -10000;
+            }
+            int randInt = RandomNumberGenerator.GetInt32(min, max);
+            return randInt;
+        }
+
+
+        /// <summary>RandomNumberGenerator를 이용, 범위 안의 난수를 반환하는 함수</summary>
+        private float RandomRange(float min, float max)
+        {
+            if (max < min)
+            {
+                Debug.LogError("Min값이 Max값보다 높습니다.");
+                return -1;
+            }
+
+            byte[] bytes = new byte[4];
+            
+            using(RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            float randValue = BitConverter.ToSingle(bytes, 0);
+            float range = max - min;
+            float rendFloat = (randValue * range) + min;
+
+            return rendFloat;
+        }
+
     }
 }
